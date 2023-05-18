7bda0f3692e7d9523d8d81512f9f8e5ac5e0a544
aboyer <118172356+PetitHommeLeVrai@users.noreply.github.com>
2023-05-18 16:58:37 +0200

    Add files via upload

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,17 +6,18 @@
 /*   By: aboyer <aboyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 13:29:15 by aboyer            #+#    #+#             */
-/*   Updated: 2023/05/18 14:59:47 by aboyer           ###   ########.fr       */
+/*   Updated: 2023/05/18 16:51:55 by aboyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "tools.h"
+# include "libft.h"
 # include "mlx.h"
 # include "mlx_int.h"
-
+# include "tools.h"
+# include "window.h"
 # include <fcntl.h>
 # include <sys/stat.h>
 
--- includes/window.h
+++ includes/window.h
@@ -1,26 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   window.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aboyer <aboyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/18 14:28:56 by aboyer            #+#    #+#             */
-/*   Updated: 2023/05/18 16:45:22 by aboyer           ###   ########.fr       */
+/*   Created: 2023/05/18 16:49:56 by aboyer            #+#    #+#             */
+/*   Updated: 2023/05/18 16:51:37 by aboyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#ifndef WINDOW_H
+# define WINDOW_H
 
-int main(int ac, char **av)
-{
-	t_data data;
+int	init(t_data *data, int ac, char **av);
+int	destroy_win(t_data *data);
+int	draw(t_data *data);
+int	handle_keyrelease(int keysym, t_data *data);
+int	handle_input(int keysym, t_data *data);
 
-	if (ac != 2)
-	{
-		printf("Error\nOnly 1 argument is needed\n");
-		return (1);
-	}
-	if (init(&data, ac, av) == 0)
-		return (1);
-}
+#endif
