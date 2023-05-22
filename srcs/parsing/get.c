98a9a61d53788c73e391dc5c80ca4f64dedd61f5
Mohamed MEGUEDMINI <mmeguedm@e3r6p16.clusters.42paris.fr>
2023-05-22 16:34:40 +0200

    tmp

--- srcs/parsing/get.c
+++ srcs/parsing/get.c
@@ -6,11 +6,16 @@
 /*   By: mmeguedm <mmeguedm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 14:57:32 by mmeguedm          #+#    #+#             */
-/*   Updated: 2023/05/22 15:26:49 by mmeguedm         ###   ########.fr       */
+/*   Updated: 2023/05/22 16:32:52 by mmeguedm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	get_position_player(t_data *data)
+#include "cub3d.h"
+
+void	get_map(t_data *data)
 {
-	
+}
+
+void	get_position_player(t_data *data)
+{	
 }
