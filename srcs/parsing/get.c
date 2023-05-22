f7ceecf3c58a44b734c9385f8566acb823eedd2d
Mohamed MEGUEDMINI <mmeguedm@e3r6p16.clusters.42paris.fr>
2023-05-22 17:23:30 +0200

    add ftc

--- srcs/parsing/get.c
+++ srcs/parsing/get.c
@@ -6,14 +6,22 @@
 /*   By: mmeguedm <mmeguedm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 14:57:32 by mmeguedm          #+#    #+#             */
-/*   Updated: 2023/05/22 16:35:06 by mmeguedm         ###   ########.fr       */
+/*   Updated: 2023/05/22 17:16:58 by mmeguedm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	get_position_player(t_data *data)
+void	get_map(t_data *data)
 {
+	int	i;
+
+	i = 0;
+	while (data->img.map[i])
+	{
+		printf("data->img.map : %s\n", data->img.map[i]);
+		i++;
+	}
 }
 
 void	get_position_player(t_data *data)
